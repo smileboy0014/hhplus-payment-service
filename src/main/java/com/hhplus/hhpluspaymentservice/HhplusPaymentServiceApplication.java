042090179064b5e9c58c619dcbc9fa1a7df652a5
gt.park <smileboy0014@gmail.com>
2024-09-05 16:30:44 +0900

    first commit

---
+++ src/main/java/com/hhplus/hhpluspaymentservice/HhplusPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hhplus.hhpluspaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusPaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhplusPaymentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hhplus/hhpluspaymentservice/HhplusPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhplus.hhpluspaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusPaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
